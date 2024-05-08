8a185fa1f53a255ca237f4218d37050f110b17a5
Radosław Wróbel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-05-08 13:18:29 +0000

    yes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+
 /*
 Kod bazowy programu Commit4_0: 
 • Program dodaje do prostej bazy danych (pliku db.txt) dane odnośnie Studentów.
@@ -6,22 +7,51 @@ Kod bazowy programu Commit4_0:
 • Klasa Service obsługuje odczyt i zapis do pliku bazy danych.
 • Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
 */
-
 import java.io.IOException;
+import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    try {
-      Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+    Scanner scanner = new Scanner(System.in);
+    Service s = new Service();
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
+    boolean programDziala = true;
+
+    while (programDziala) {
+      System.out.println("Wybierz opcję:");
+      System.out.println("1. Wyświetl zapisane dane studentów");
+      System.out.println("2. Dodaj nowego studenta");
+      System.out.println("0. Wyjdź z programu");
+
+      int wybor = scanner.nextInt();
+
+      try {
+        switch (wybor) {
+          case 1:
+            System.out.println("Zapisane dane studentów:");
+            for (Student student : s.getStudents()) {
+              System.out.println(student.ToString());
+            }
+            break;
+          case 2:
+            System.out.println("Podaj imię nowego studenta:");
+            String imie = scanner.next();
+            System.out.println("Podaj wiek nowego studenta:");
+            int wiek = scanner.nextInt();
+            s.addStudent(new Student(imie, wiek));
+            System.out.println("Nowy student został dodany.");
+            break;
+          case 0:
+            System.out.println("Program zostanie zamknięty.");
+            programDziala = false;
+            break;
+          default:
+            System.out.println("Niepoprawny wybór. Wybierz ponownie.");
+        }
+      } catch (IOException e) {
+        System.out.println("Wystąpił błąd podczas operacji na pliku.");
       }
-    } catch (IOException e) {
-
     }
+    scanner.close();
   }
 }
