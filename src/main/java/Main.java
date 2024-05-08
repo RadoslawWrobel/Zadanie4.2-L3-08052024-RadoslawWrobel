25b09c941c966f1dc6e909e75a38681fed815098
Radosław Wróbel <161605563+RadoslawWrobel@users.noreply.github.com>
2024-05-08 13:23:34 +0000

    Zadanie 4.2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,8 @@ class Main {
 
     while (programDziala) {
       System.out.println("Wybierz opcję:");
-      System.out.println("1. Dodaj nowego studenta");
+      System.out.println("1. Wyświetl zapisane dane studentów");
+      System.out.println("2. Dodaj nowego studenta");
       System.out.println("0. Wyjdź z programu");
 
       int wybor = scanner.nextInt();
@@ -27,6 +28,12 @@ class Main {
       try {
         switch (wybor) {
           case 1:
+            System.out.println("Zapisane dane studentów:");
+            for (Student student : s.getStudents()) {
+              System.out.println(student.ToString());
+            }
+            break;
+          case 2:
             System.out.println("Podaj imię nowego studenta:");
             String imie = scanner.next();
             System.out.println("Podaj wiek nowego studenta:");
